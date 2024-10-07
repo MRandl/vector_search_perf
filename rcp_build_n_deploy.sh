21a673306693cda2428850b01595c5483d822b49
Mathis Randl <mathis.randl@epfl.ch>
2024-10-07 15:11:29 +0200

    update for cluster deployment

---
+++ rcp_build_n_deploy.sh
@@ -0,0 +1,8 @@
+docker build -t ic-registry.epfl.ch/sacs/randl/vectors_perf ./docker
+docker push ic-registry.epfl.ch/sacs/randl/vectors_perf
+runai_rcp submit --name vecperf \
+    -i ic-registry.epfl.ch/sacs/randl/vectors_perf  \
+    --gpu 0 --cpu 32 \
+    --pvc runai-sacs-randl-scratch:/mnt/nfs \
+    --interactive -- \"sleep infinity\"
+
