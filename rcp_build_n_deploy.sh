074ed707ada051a9917da5e508e4fe123b1f7731
Mathis Randl <mathis.randl@epfl.ch>
2024-10-13 10:13:20 +0200

    patch for longevity of container 2

--- rcp_build_n_deploy.sh
+++ rcp_build_n_deploy.sh
@@ -3,6 +3,7 @@ docker push ic-registry.epfl.ch/sacs/randl/vectors_perf
 runai_rcp submit --name vecperf \
     -i ic-registry.epfl.ch/sacs/randl/vectors_perf  \
     --gpu 0 --cpu 32 \
+    --memory 450G \
     --pvc runai-sacs-randl-scratch:/mnt/nfs \
     --interactive -- \"sleep infinity\"
 
--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker build -t vectors ./docker && docker run -it --cap-add PERFMON --rm -v /home/matrix/Documents/disk:/dochost --runtime=nvidia --gpus all vectors
+docker build -t vectors ./docker && docker run -it --rm -v /home/matrix/Documents/disk:/dochost --runtime=nvidia --gpus all vectors
